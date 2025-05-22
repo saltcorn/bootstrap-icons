c3c2d32a0c118d1b50291d95b77bce37c193d998
gerard1001 <rutayisiregerard2002@gmail.com>
2025-05-22 14:58:04 +0200

    update: including version & CDN styles

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const Workflow = require("@saltcorn/data/models/workflow");
-const Form = require("@saltcorn/data/models/form");
 const icons = require("./bootstrap-icons.json");
 
 module.exports = {
@@ -7,51 +5,10 @@ module.exports = {
   plugin_name: "bootstrap-icons",
   headers: [
     {
-      css: "/plugins/public/bootstrap-icons/bootstrap-icons.css",
+      css: `/plugins/public/bootstrap-icons@${
+        require("./package.json").version
+      }/bootstrap-icons.css`,
     },
   ],
   icons: icons.map((icon) => `bi bi-${icon}`),
-  configuration_workflow: () =>
-    new Workflow({
-      steps: [
-        {
-          name: "Bootstrap Icon configuration",
-          form: () =>
-            new Form({
-              fields: [
-                {
-                  name: "enabled_icons",
-                  label: "Enabled Icons",
-                  type: "String",
-                  sublabel:
-                    "Comma-separated list of icons to enable. If not provided, all icons will be enabled.",
-                  required: false,
-                  attributes: {
-                    options: icons.join(", "),
-                  },
-                },
-                {
-                  name: "css_path",
-                  label: "Custom CSS Path",
-                  type: "String",
-                    sublabel:
-                        "Path to custom CSS file. If not provided, the default Bootstrap Icons CSS will be used.",
-                  required: false,
-                },
-              ],
-            }),
-        },
-      ],
-    }),
-
-  onLoad: async (config) => {
-    if (config.css_path) {
-      module.exports.headers[0].css = config.css_path;
-    }
-    if (config.enabled_icons) {
-      module.exports.icons = config.enabled_icons
-        .split(",")
-        .map((icon) => `bi bi-${icon.trim()}`);
-    }
-  },
 };
